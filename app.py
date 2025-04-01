1dcaefedec1844503e9e04e76722ee2190eba4e7
hetbhalani <bhalanihet2006@gmail.com>
2025-04-01 07:30:39 +0530

    change

--- app.py
+++ app.py
@@ -97,4 +97,5 @@ def getMovies():
     return jsonify({'movies': movies_list}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  
+    app.run(debug=True, host='0.0.0.0', port=port)
